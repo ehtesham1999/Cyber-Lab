dcd74df7ceda8fcd1380feece83d2e5bf91d47b8
ehtesham1999 <43861550+ehtesham1999@users.noreply.github.com>
2020-12-16 13:27:51 +0530

    Update Diffie.java

--- Diffie.java
+++ Diffie.java
@@ -3,62 +3,50 @@ import java.math.*;
 
 public class Diffie {
 
-	static int gcd(int a , int b)
-	{
-		if(a==0)
-			return b;
-		else
-			return gcd(b%a,a);
-	}
-
-
-	static long power(long a , long b , long p)
-	{
-		return ((long)(Math.pow(a,b))%p);
-	}
-
-
-
+	
+    static BigInteger power(BigInteger A , BigInteger b , BigInteger P)
+    {
+        
+        return (A.pow(b.intValue())).mod(P);
+        
+    }
 
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        long xa,xb;
-        long p,g;
-        p = 7;
-        g = 17;
-        long ya = 0,yb = 0;
-        System.out.println(" P : "+p);
-        System.out.println(" G : "+g);
+        BigInteger xa,xb;
+        BigInteger p,g;
+        // sample P value = 353 and sample G value = 3 
+        // G is a primitive root of 353 
+        // Xa<P and Xb<P
+        p = BigInteger.valueOf(353);
+        g = BigInteger.valueOf(3);
+        BigInteger ya ,yb ;
+        System.out.println(" P : "+p.intValue());
+        System.out.println(" G : "+g.intValue());
         
         System.out.println("Enter Xa(<p): ");
-        xa = scan.nextInt();
+        xa = BigInteger.valueOf(scan.nextInt());
 
         ya = power(g,xa,p);
-        System.out.println(" Ya : "+ya);
+        System.out.println(" Ya : "+ya.intValue());
         
 
 
         System.out.println("Enter Xb(<p): ");
-        xb = scan.nextInt();
+        xb = BigInteger.valueOf(scan.nextInt());
         
         yb = power(g,xb,p);
-        System.out.println(" Yb : "+yb);
+        System.out.println(" Yb : "+yb.intValue());
 
-        long ka,kb;
+        BigInteger ka,kb;
 
         ka = power(yb,xa,p);
         kb = power(ya,xb,p);
-		System.out.println("key Ka : "+ka);
-		System.out.println("key Kb : "+kb);
+		System.out.println("key Ka : "+ka.intValue());
+		System.out.println("key Kb : "+kb.intValue());
 		        
 
-
-
-
-
-
-        
     }
 }
