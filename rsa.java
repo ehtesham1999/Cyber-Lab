b2090e8c0d301b5a563dcedfe80261248a7cf190
ehtesham1999 <43861550+ehtesham1999@users.noreply.github.com>
2020-12-16 14:12:47 +0530

    Update rsa.java

--- rsa.java
+++ rsa.java
@@ -1,18 +1,37 @@
-import java.util.*;
 import java.math.*;
-
-public class rsa_practise {
-
-	static int gcd(int a , int b)
-	{
-		if(a==0)
-			return b;
-		else
-			return gcd(b%a,a);
-	}
+import java.util.*;
 
 
-	static int modInverse(int a, int modm)
+class rsa {
+
+   static int gcd(int a , int b)
+   {
+      if(a==0)
+         return b;
+      else
+         return gcd(b%a,a);
+   }
+
+   static int randprime()
+   {
+      // create a BigInteger object
+      BigInteger bi;
+
+      // create and assign value to bitLength
+      int bitLength = 4;
+
+      // create a random object
+      Random rnd = new Random();
+
+      // assign probablePrime result to bi using bitLength and rnd
+      // static method is called using class name
+      bi = BigInteger.probablePrime(bitLength, rnd);
+      return bi.intValue();
+
+
+   }
+
+   static int modInverse(int a, int modm)
     {
         a = a % modm;
         for (int x = 1; x < modm; x++)
@@ -30,54 +49,56 @@ public class rsa_practise {
         Scanner reader = new Scanner(System.in);
 
 
-        int p,q,n=0,phi,d=0,e=0,i;
-		 int msg=0;
-		 double c=0;
-		 BigInteger msgback;
-		 BigInteger N;
-		 BigInteger C;
-		System.out.print("Enter msg: ");
-        msg = reader.nextInt(); 
-        System.out.print("Enter P: ");
-        p = reader.nextInt();
-        System.out.print("Enter Q: ");
-		q = reader.nextInt();
+         int p,q,n=0,phi,d=0,e=0,i;
+         int msg=0;
+         double c=0;
+         BigInteger msgback;
+         BigInteger N;
+         BigInteger C;
+         System.out.print("Enter msg: ");
+         msg = reader.nextInt(); 
+         // System.out.print("Enter P: ");
+         // p = reader.nextInt();
+         // System.out.print("Enter Q: ");
+         // q = reader.nextInt();
 
-		n = p*q;
-		phi = (p-1)*(q-1);
+         p = randprime();
+         q = randprime();
 
-		System.out.println("N : "+n);
-		System.out.println("Phi : "+phi);
-
-		for(e=7;e<phi;e++)
-		{
-			if(gcd(e,phi)==1)
-			{
-				break;
-			}
-		}
-
-		System.out.println("e: "+e);
-		d = modInverse(e,phi);
-
-		System.out.println("d: "+d);
-
-		c = Math.pow(msg,e)%n;
-
-		System.out.println("cipher C: "+c);
-
-		N = BigInteger.valueOf(n);
-		C = BigDecimal.valueOf(c).toBigInteger();
-
-		msgback = (C.pow(d)).mod(N);
-
-		System.out.println("Decrypted msg: "+msgback);
+         System.out.println("Random prime P = "+p);
+         System.out.println("Random prime Q = "+q);
 
 
+         n = p*q;
+         phi = (p-1)*(q-1);
+
+         System.out.println("N : "+n);
+         System.out.println("Phi : "+phi);
+
+         for(e=7;e<phi;e++)
+         {
+            if(gcd(e,phi)==1)
+            {
+               break;
+            }
+         }
+
+         System.out.println("e: "+e);
+         d = modInverse(e,phi);
+
+         System.out.println("d: "+d);
+
+         c = Math.pow(msg,e)%n;
+
+         System.out.println("cipher C: "+c);
+
+         N = BigInteger.valueOf(n);
+         C = BigDecimal.valueOf(c).toBigInteger();
+
+         msgback = (C.pow(d)).mod(N);
+
+         System.out.println("Decrypted msg: "+msgback);
 
 
-
-
-        
     }
 }
